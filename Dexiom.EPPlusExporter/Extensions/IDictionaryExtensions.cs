a7fb0697662b74df499da21f2f290f53189101d1
Jonathan Paré <jpare@dexiom.com>
2017-01-28 10:05:54 -0500

    Add "AddOrUpdate" dictionary extension

---
+++ Dexiom.EPPlusExporter/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Dexiom.EPPlusExporter.Extensions
+{
+    internal static class IDictionaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+                dictionary[key] = value;
+            else
+                dictionary.Add(key, value);
+        }
+    }
+}
